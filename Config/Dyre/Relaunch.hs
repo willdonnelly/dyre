8199c124c64894752f81a819edc07a510b86e537
Will Donnelly <will.donnelly@gmail.com>
2009-08-04 22:52:47 -0500

    Removed 'getProgName', since it's useless.

--- Config/Dyre/Relaunch.hs
+++ Config/Dyre/Relaunch.hs
@@ -5,12 +5,11 @@ module Config.Dyre.Relaunch
   , restoreState
   ) where
 
-import Data.Maybe           ( fromMaybe )
+import Data.Maybe           ( fromMaybe, fromJust )
 import System.IO            ( writeFile, readFile )
 import System.IO.Error      ( try )
 import System.FilePath      ( (</>) )
 import System.Directory     ( getTemporaryDirectory, removeFile )
-import System.Environment   ( getProgName )
 import System.Posix.Process ( executeFile, getProcessID )
 
 import System.IO.Storage    ( putValue, delValue )
@@ -19,28 +18,18 @@ import Config.Dyre.Options  ( customOptions, getMasterBinary, getStatePersist )
 relaunchMaster :: Maybe [String] -> IO ()
 relaunchMaster otherArgs = do
     args <- customOptions otherArgs
-
-    -- Get the program name and path
-    masterName <- getProgName
-    masterPath <- getMasterBinary
-
-    -- If we have a path, run it, otherwise hope the name works
-    case masterPath of
-         Nothing -> executeFile masterName True args Nothing
-         Just mp -> executeFile mp False args Nothing
+    masterPath <- fmap fromJust getMasterBinary
+    executeFile masterPath False args Nothing
 
 relaunchWithState :: Show a => a -> Maybe [String] -> IO ()
 relaunchWithState state otherArgs = do
     -- Calculate the path to the state file
-    progName <- getProgName
     procID   <- getProcessID
     tempDir  <- getTemporaryDirectory
-    let statePath = tempDir </> progName ++ "-" ++ (show procID) ++ ".state"
-
+    let statePath = tempDir </> (show procID) ++ ".state"
     -- Write the state to the file and store the path
-    writeFile statePath (show state)
+    writeFile statePath . show $ state
     putValue "dyre" "persistState" statePath
-
     -- Relaunch
     relaunchMaster otherArgs
 
