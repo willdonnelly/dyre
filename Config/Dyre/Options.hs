bde69498f46eb5bab4868e81a676823e518e7417
Will Donnelly <will.donnelly@gmail.com>
2009-08-04 22:51:06 -0500

    Tweaked masterBinary path handling.

--- Config/Dyre/Options.hs
+++ Config/Dyre/Options.hs
@@ -17,11 +17,22 @@ import System.Environment.Executable
 --   and then execute the provided IO action with all Dyre's
 --   options removed from the arguments.
 withDyreOptions action = withStore "dyre" $ do
+    -- Pretty important
     args <- getArgs
-    storeFlag args "--dyre-state-persist=" "persistState"
+
+    -- If the flag exists, it overrides the current file. Likewise,
+    --   if it doesn't exist, we end up with the path to our current
+    --   file. This seems like a sensible way to do it.
+    this <- getExecutablePath
+    putValue "dyre" "masterBinary" this
     storeFlag args "--dyre-master-binary=" "masterBinary"
+
+    -- Load the other important arguments into IO storage.
+    storeFlag args "--dyre-state-persist=" "persistState"
     putValue "dyre" "forceReconf"  $ "--force-reconf" `elem` args
     putValue "dyre" "debugMode"    $ "--dyre-debug"   `elem` args
+
+    -- We filter the arguments, so now Dyre's arguments 'vanish'
     withArgs (filterArgs args) action
   where filterArgs = filter $ not . prefixElem dyreArgs
         prefixElem xs = or . zipWith ($) (map isPrefixOf xs) . repeat
@@ -40,8 +51,7 @@ getStatePersist = getValue "dyre" "persistState"
 
 customOptions :: Maybe [String] -> IO [String]
 customOptions otherArgs = do
-    binaryPath <- getExecutablePath
-    masterPath <- getDefaultValue "dyre" "masterBinary" binaryPath
+    masterPath <- getMasterBinary
     stateFile  <- getStatePersist
     debugMode  <- getDebug
     return . filter (not . null) $ fromMaybe [] otherArgs ++
@@ -49,7 +59,7 @@ customOptions otherArgs = do
         , case stateFile of
                Nothing -> ""
                Just sf -> "--dyre-state-persist=" ++ sf
-        , "--dyre-master-binary=" ++ masterPath
+        , "--dyre-master-binary=" ++ (fromJust masterPath)
         ]
 
 -- | Look for the given flag in the argument array, and store
