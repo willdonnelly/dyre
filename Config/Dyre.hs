1c9bc8393824a9cb403f87e595ffb01d8477d60c
Will Donnelly <will.donnelly@gmail.com>
2009-09-12 10:06:56 -0500

    Updated documentation.

--- Config/Dyre.hs
+++ Config/Dyre.hs
@@ -18,69 +18,68 @@ program (recompiling if applicable), and persist state across restarts,
 but it has no impact whatsoever on the rest of the library whether it
 is used or not.
 
-A full example of using Dyre's recompilation and relaunching features
-is as follows:
+A full example of using most of Dyre's major features is as follows:
 
->-- DyreExample.hs --
->module DyreExample where
->
->import qualified Config.Dyre as Dyre
->
->import Config.Dyre.Relaunch
->import System.IO
->
->data Config = Config { message :: String, errorMsg :: Maybe String }
->data State  = State { bufferLines :: [String] } deriving (Read, Show)
->
->defaultConfig :: Config
->defaultConfig = Config "Dyre Example v0.1" Nothing
->
->showError :: Config -> String -> Config
->showError cfg msg = cfg { errorMsg = Just msg }
->
->realMain Config{message = message, errorMsg = errorMsg } = do
->    (State buffer) <- restoreTextState $ State []
->    case errorMsg of
->         Nothing -> return ()
->         Just em -> putStrLn $ "Error: " ++ em
->    putStrLn message
->    mapM putStrLn . reverse $ buffer
->    putStr "> " >> hFlush stdout
->    input <- getLine
->    case input of
->         "exit" -> return ()
->         "quit" -> return ()
->         other  -> relaunchWithTextState (State $ other:buffer) Nothing
->
->dyreExample = Dyre.wrapMain $ Dyre.defaultParams
->    { Dyre.projectName = "dyreExample"
->    , Dyre.realMain    = realMain
->    , Dyre.showError   = showError
->    }
+    -- DyreExample.hs --
+    module DyreExample where
+
+    import qualified Config.Dyre as Dyre
+    import Config.Dyre.Relaunch
+
+    import System.IO
+
+    data Config = Config { message :: String, errorMsg :: Maybe String }
+    data State  = State { bufferLines :: [String] } deriving (Read, Show)
+
+    defaultConfig :: Config
+    defaultConfig = Config "Dyre Example v0.1" Nothing
+
+    showError :: Config -> String -> Config
+    showError cfg msg = cfg { errorMsg = Just msg }
+
+    realMain Config{message = message, errorMsg = errorMsg } = do
+        (State buffer) <- restoreTextState $ State []
+        case errorMsg of
+             Nothing -> return ()
+             Just em -> putStrLn $ "Error: " ++ em
+        putStrLn message
+        mapM putStrLn . reverse $ buffer
+        putStr "> " >> hFlush stdout
+        input <- getLine
+        case input of
+             "exit" -> return ()
+             "quit" -> return ()
+             other  -> relaunchWithTextState (State $ other:buffer) Nothing
+
+    dyreExample = Dyre.wrapMain $ Dyre.defaultParams
+        { Dyre.projectName = "dyreExample"
+        , Dyre.realMain    = realMain
+        , Dyre.showError   = showError
+        }
 
 Notice that all of the program logic is contained in the 'DyreExample'
-module. The main module of the program is absolutely trivial.
+module. The main module of the program is absolutely trivial, being
+essentially just the default configuration for the program:
 
->-- Main.hs --
->module Main where
->import DyreExample
->main = dyreExample defaultConfig
+    -- Main.hs --
+    import DyreExample
+    main = dyreExample defaultConfig
 
-When reading the above program, bear in mind exactly how much of the
+When reading the above program, notice that the majority of the
 code is simply *program logic*. Dyre is designed to intelligently
-handle recompilation with a bare minimum of program modification.
+handle recompilation with a minimum of programmer work.
 
 Some mention should be made of Dyre's defaults. The 'defaultParams'
 structure used in the example defines reasonable default values for
-several configuration items. In the absence of any other definitions,
-Dyre will default to outputting status messages to stderr, not hiding
-any packages during compilation, and passing no special options to GHC.
+most configuration items. The three elements defined above are the
+only elements that must be overridden. For documentation of the
+parameters, consult the 'Config.Dyre.Params' module.
 
-Also, Dyre will expect configuration files to be placed at the path
-'$XDG_CONFIG_HOME/<app>/<app>.hs', and it will store cache files in
-the '$XDG_CACHE_HOME/<app>/' directory. The 'System.Environment.XDG'
-module will be used to determine these paths, so refer to it for
-behaviour on Windows platforms.
+In the absence of any customization, Dyre will search for configuration
+files in '$XDG_CONFIG_HOME/<appName>/<appName>.hs', and will store
+cache files in '$XDG_CACHE_HOME/<appName>/' directory. The module
+'System.Environment.XDG' is used for this purpose, which also provides
+analogous behaviour on Windows.
 -}
 module Config.Dyre ( wrapMain, Params(..), defaultParams ) where
 
@@ -94,14 +93,8 @@ import Config.Dyre.Compat  ( customExec )
 import Config.Dyre.Options ( getForceReconf, getDenyReconf, getDebug, withDyreOptions )
 import Config.Dyre.Paths   ( getPaths, maybeModTime )
 
--- | A set of reasonable defaults for configuring Dyre. If the minimal set of
---   fields are modified, the program will use the XDG-defined locations for
---   configuration and cache files (see 'System.Environment.XDG.BaseDir' for
---   details), pass no special options to GHC, and will output status messages
---   to stderr.
---
---   The fields that will have to be filled are 'projectName', 'realMain', and
---   'showError'
+-- | A set of reasonable defaults for configuring Dyre. The fields that
+--   have to be filled are 'projectName', 'realMain', and 'showError'.
 defaultParams :: Params cfgType
 defaultParams = Params
     { projectName  = undefined
