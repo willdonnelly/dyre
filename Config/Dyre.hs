5a188d2bf3ec2b12cf6b09fb4a2a102fb50272d6
Joe Edmonds <joe@elem.com>
2009-10-24 09:49:03 -0600

    Put code in code block so it gets formatted properly by haddock.

--- Config/Dyre.hs
+++ Config/Dyre.hs
@@ -20,50 +20,50 @@ is used or not.
 
 A full example of using most of Dyre's major features is as follows:
 
-    -- DyreExample.hs --
-    module DyreExample where
-
-    import qualified Config.Dyre as Dyre
-    import Config.Dyre.Relaunch
-
-    import System.IO
-
-    data Config = Config { message :: String, errorMsg :: Maybe String }
-    data State  = State { bufferLines :: [String] } deriving (Read, Show)
-
-    defaultConfig :: Config
-    defaultConfig = Config "Dyre Example v0.1" Nothing
-
-    showError :: Config -> String -> Config
-    showError cfg msg = cfg { errorMsg = Just msg }
-
-    realMain Config{message = message, errorMsg = errorMsg } = do
-        (State buffer) <- restoreTextState $ State []
-        case errorMsg of
-             Nothing -> return ()
-             Just em -> putStrLn $ "Error: " ++ em
-        putStrLn message
-        mapM putStrLn . reverse $ buffer
-        putStr "> " >> hFlush stdout
-        input <- getLine
-        case input of
-             "exit" -> return ()
-             "quit" -> return ()
-             other  -> relaunchWithTextState (State $ other:buffer) Nothing
-
-    dyreExample = Dyre.wrapMain $ Dyre.defaultParams
-        { Dyre.projectName = "dyreExample"
-        , Dyre.realMain    = realMain
-        , Dyre.showError   = showError
-        }
+>    -- DyreExample.hs --
+>    module DyreExample where
+>
+>    import qualified Config.Dyre as Dyre
+>    import Config.Dyre.Relaunch
+>
+>    import System.IO
+>
+>    data Config = Config { message :: String, errorMsg :: Maybe String }
+>    data State  = State { bufferLines :: [String] } deriving (Read, Show)
+>
+>    defaultConfig :: Config
+>    defaultConfig = Config "Dyre Example v0.1" Nothing
+>
+>    showError :: Config -> String -> Config
+>    showError cfg msg = cfg { errorMsg = Just msg }
+>
+>    realMain Config{message = message, errorMsg = errorMsg } = do
+>        (State buffer) <- restoreTextState $ State []
+>        case errorMsg of
+>             Nothing -> return ()
+>             Just em -> putStrLn $ "Error: " ++ em
+>        putStrLn message
+>        mapM putStrLn . reverse $ buffer
+>        putStr "> " >> hFlush stdout
+>        input <- getLine
+>        case input of
+>             "exit" -> return ()
+>             "quit" -> return ()
+>             other  -> relaunchWithTextState (State $ other:buffer) Nothing
+>
+>    dyreExample = Dyre.wrapMain $ Dyre.defaultParams
+>        { Dyre.projectName = "dyreExample"
+>        , Dyre.realMain    = realMain
+>        , Dyre.showError   = showError
+>        }
 
 Notice that all of the program logic is contained in the 'DyreExample'
 module. The main module of the program is absolutely trivial, being
 essentially just the default configuration for the program:
 
-    -- Main.hs --
-    import DyreExample
-    main = dyreExample defaultConfig
+>    -- Main.hs --
+>    import DyreExample
+>    main = dyreExample defaultConfig
 
 When reading the above program, notice that the majority of the
 code is simply *program logic*. Dyre is designed to intelligently
