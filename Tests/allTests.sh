8f98b0ef86c1b68f42d92d832618a7bf188ea2d8
Will Donnelly <wgd@willdonnelly.net>
2012-10-14 14:07:07 -0500

    That was a dumb oversight

--- Tests/allTests.sh
+++ Tests/allTests.sh
@@ -7,7 +7,6 @@ for TESTDIR in `find . -mindepth 1 -type d`; do
     TEST_RESULT=`./runTest.sh`
     if [ "$TEST_RESULT" != 'Passed' ]; then
         echo "$TEST_RESULT in test $TESTDIR"
-        rm -r working
         exit 1
     fi
     cd ..
