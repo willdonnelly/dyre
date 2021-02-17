4e18824f85f5d65e7d217c19cb9049975d29305f
Fraser Tweedale <frase@frase.id.au>
2021-02-18 00:59:54 +1000

    ci: invoke script via "sh"
    Windows does not complain about the incantation "./allTests.sh".
    But it doesn't execute it either.
    
    Invoke the test script via explicit shell.

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,7 @@
 import System.Directory (setCurrentDirectory)
-import System.FilePath ((</>))
 import System.Process (callCommand)
 
 main :: IO ()
 main = do
   setCurrentDirectory "Tests"
-  callCommand ("." </> "allTests.sh")
+  callCommand "sh allTests.sh"
